7c642522135ee516a884ba0a23f11c18018629cd
SHASHI-17 <hibye31583@gmail.com>
2024-04-04 23:37:04 +0530

    webhook fixed

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,22 +1,45 @@
+// import mongoose from "mongoose";
+
+// const MONGODB_URI=process.env.MONGODB_URI;
+// console.log(MONGODB_URI);
+
+
+// let cached = (global as any).mongoose || {conn:null,promise:null};
+
+// export const connectToDatabase = async () => {
+//   console.log('aa rha');
+  
+//     if (cached.conn) return cached.conn;
+  
+//     if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
+  
+//     cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
+//       dbName: 'evently',
+//       bufferCommands: false,
+//     })
+  
+//     cached.conn = await cached.promise;
+  
+//     return cached.conn;
+//   }
+
 import mongoose from "mongoose";
 
-const MONGODB_URI=process.env.MONGODB_URI;
-console.log(MONGODB_URI);
+let isConnected = false;
 
-
-let cached = (global as any).mongoose || {conn:null,promise:null};
-
-export const connectToDatabase = async () => {
-    if (cached.conn) return cached.conn;
-  
-    if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
-  
-    cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
-      dbName: 'evently',
-      bufferCommands: false,
-    })
-  
-    cached.conn = await cached.promise;
-  
-    return cached.conn;
-  }
+export const connectToDatabase  = async () =>{
+    mongoose.set('strictQuery',true);
+    if(isConnected){
+        console.log('MongoDB is already connected');
+        return;
+    }
+    try {
+        await mongoose.connect(process.env.MONGODB_URI!,{
+            dbName: "evently",
+          });
+          isConnected = true;
+          console.log('MongoDB connected')
+    } catch (e:unknown) {
+        console.log(e);
+    }
+}
