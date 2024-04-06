09ccd353a32ca05ecf613ca9aaea695deaa562cc
SHASHI-17 <hibye31583@gmail.com>
2024-04-07 00:08:34 +0530

    stripe integration

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -3,6 +3,7 @@ import { formatDateTime } from "@/lib/utils";
 import { SearchParamProps } from "@/types";
 import Image from "next/image";
 import Collection from '@/components/shared/Collection';
+import CheckoutButton from "@/components/shared/CheckoutButton";
 
 const EventDetails = async ({ params: { id } ,searchParams}: SearchParamProps) => {
   const event = await getEventById(id);
@@ -48,7 +49,7 @@ const EventDetails = async ({ params: { id } ,searchParams}: SearchParamProps) =
               </div>
             </div>
 
-            {/* <CheckoutButton event={event} /> */}
+            <CheckoutButton event={event} />
 
             <div className="flex flex-col gap-5">
               <div className="flex gap-2 md:gap-3">
